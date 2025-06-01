124c4bb54e8303156ca8a9cbfde21970112a07ac
YunusIJ <yunusisholaj@gmail.com>
2025-06-01 12:49:13 +0100

    comment controller/route

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,41 +1,48 @@
 const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    issue: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Issue',
-        required: true
-    },
-    likes: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
-    replies: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Comment'
-    }],
-    parentComment: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Comment',
-        default: null
-    },
-    isEdited: {
-        type: Boolean,
-        default: false
-    }
-}, 
-{
-    timestamps: true
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  issue: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Issue',
+    required: true
+  },
+  replies: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
+  parentComment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+    default: null
+  },
+  upvotes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  isEdited: {
+    type: Boolean,
+    default: false
+  },
+  isAnonymous: {
+    type: Boolean,
+    default: false
+  },
+  displayName: {
+    type: String,
+    default: null
+  }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
