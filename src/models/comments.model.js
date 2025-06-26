c4e656412e269ad8f8e7a11820db5f0ee0a51058
unknown <ola.gabriel19@gmail.com>
2025-06-26 12:42:16 +0100

    updating tayo branch

---
+++ src/models/comments.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const CommentSchema = new mongoose.Schema({
+  content: {type: String, required: true, trim: true},
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  issue: {type: mongoose.Schema.Types.ObjectId, ref: 'Issue', required: true},
+  replies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}],
+  parentComment: {type: mongoose.Schema.Types.ObjectId, ref: 'Comment', default: null},
+  upvotes: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  isEdited: {type: Boolean, default: false},
+  isAnonymous: {type: Boolean, default: false},
+  displayName: {type: String, default: null}
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
