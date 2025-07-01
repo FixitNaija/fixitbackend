cddc7421350b94ae2b5eb6b4491054d503ed45cd
YunusIJ <yunusisholaj@gmail.com>
2025-07-01 14:20:17 +0100

    fixed google authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     firstName: {type: String},
     lastName: {type: String},
     email: {type: String, unique: true},
-    password: {type: String, required: true},
+    password: {type: String,},
     otp: {type: String, default: null},
     isVerified: {type: Boolean, default: false},
     state: {type: String},
