2ba7489941e88e01f575e5f1ef1bb15d9cbc015e
unknown <ola.gabriel19@gmail.com>
2025-07-07 14:32:13 +0300

    updated HTTP method for verifyUser

--- src/routers/user/user.router.js
+++ src/routers/user/user.router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/signup', userSignup);
 router.post('/login', userLogin);
-router.get('/verify', verifyUser);
+router.post('/verify', verifyUser);
 router.get('/resendotp', resendOTP);
 router.post('/forgotpassword', forgotPassword);
 router.post('/resetpassword', resetPassword);
